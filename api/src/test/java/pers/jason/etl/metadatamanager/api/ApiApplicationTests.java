f0b4df805fbc638fbae7dc329dd75fea8df2d82b
Jason <592623528@qq.com>
2020-02-23 00:49:23 +0800

    重构整体架构

---
+++ api/src/test/java/pers/jason/etl/metadatamanager/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.jason.etl.metadatamanager.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
