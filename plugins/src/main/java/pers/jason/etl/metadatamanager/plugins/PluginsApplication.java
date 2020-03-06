876872740791b64dd0834afbdf2349eb1e667923
Jason <592623528@qq.com>
2020-03-07 01:50:11 +0800

    增加plugin模块

---
+++ plugins/src/main/java/pers/jason/etl/metadatamanager/plugins/PluginsApplication.java
@@ -0,0 +1,13 @@
+package pers.jason.etl.metadatamanager.plugins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PluginsApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(PluginsApplication.class, args);
+  }
+
+}
---
+++ plugins/src/test/java/pers/jason/etl/metadatamanager/plugins/PluginsApplicationTests.java
@@ -0,0 +1,13 @@
+package pers.jason.etl.metadatamanager.plugins;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PluginsApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
